e71399f3f34d0c30681a45a918b91697e74ceb81
mhmdyouali <mhmd.you@yahoo.com>
2023-02-15 15:42:35 +0200

    Create Eureka Server

--- Team/src/main/java/com/spring/team/config/Proxy.java
+++ Team/src/main/java/com/spring/team/config/Proxy.java
@@ -1,9 +1,8 @@
 package com.spring.team.config;
 
 import com.spring.team.model.Team;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
