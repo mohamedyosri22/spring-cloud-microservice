753ec1c56553c9bc60e2bfd7982aba532362ce28
mhmdyouali <mhmd.you@yahoo.com>
2023-02-17 15:11:01 +0200

    Spring Load Balancer@LoadBalancerClients

--- Team/src/main/java/com/spring/team/TeamApplication.java
+++ Team/src/main/java/com/spring/team/TeamApplication.java
@@ -2,11 +2,13 @@ package com.spring.team;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.spring.team")
+@EnableDiscoveryClient
 public class TeamApplication {
 
 	public static void main(String[] args) {
